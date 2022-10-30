19fd0832cb45a8b5d4fdf81471755fcf0d51e7ae
Daniel K <komjago@outlook.com>
2022-10-30 23:01:32 +0100

    Added football world cup scoreboard implementation

--- src/main/kotlin/service/FootballWordCupScoreBoard.kt
+++ src/main/kotlin/service/FootballWordCupScoreBoard.kt
@@ -1,6 +1,7 @@
 package service
 
 import domain.Game
+import java.lang.IllegalArgumentException
 
 class FootballWordCupScoreBoard : ScoreBoard {
 
@@ -8,19 +9,26 @@ class FootballWordCupScoreBoard : ScoreBoard {
     internal val archive: MutableSet<Game> = sortedSetOf()
 
     override fun startGame(homeTeam: String, awayTeam: String) {
-        //TODO
+        if (homeTeam.equals(awayTeam, true)) throw IllegalArgumentException()
+
+        if (currentGame == null) currentGame = Game(homeTeam, awayTeam) else throw IllegalStateException()
     }
 
     override fun finishGame() {
-        //TODO
+        currentGame?.let { archive.add(it) } ?: throw IllegalStateException()
+        currentGame = null
     }
 
     override fun update(score: Pair<Int, Int>) {
-        //TODO
+        if (score.first < 0 || score.second < 0) throw IllegalArgumentException()
+
+        currentGame?.let {
+            it.homeTeamScore.score = score.first
+            it.awayTeamScore.score = score.second
+        } ?: throw IllegalStateException()
     }
 
     override fun getGamesOrderedByTotalScore(): Set<Game> {
-        //TODO
-        return emptySet()
+        return archive
     }
 }
--- src/test/kotlin/FootballWordCupScoreBoardTests.kt
+++ src/test/kotlin/FootballWordCupScoreBoardTests.kt
@@ -1,28 +1,45 @@
+import domain.Game
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.containsInRelativeOrder
+import org.hamcrest.collection.IsEmptyCollection
+import org.hamcrest.core.IsNot
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import service.FootballWordCupScoreBoard
+import java.lang.IllegalArgumentException
 
 class FootballWordCupScoreBoardTests {
 
     val footballWordCupScoreBoard = FootballWordCupScoreBoard()
 
+    private val team1Name = "test"
+    private val team2Name = "test2"
 
     @Nested
     inner class StartGame {
 
         @Test
         fun `throws IllegalArgumentException for duplicate team name`() {
-            //TODO
+            assertThrows<IllegalArgumentException> {
+                footballWordCupScoreBoard.startGame(team1Name, team1Name)
+            }
         }
 
         @Test
         fun `throws IllegalArgumentException for duplicate team name with different case`() {
-            //TODO
+            assertThrows<IllegalArgumentException> {
+                footballWordCupScoreBoard.startGame(team1Name, team1Name)
+            }
         }
 
         @Test
         fun `throws IllegalStateException if game already running`() {
-            //TODO
+            assertThrows<IllegalStateException> {
+                footballWordCupScoreBoard.startGame(team1Name, team2Name)
+                footballWordCupScoreBoard.startGame(team1Name, team2Name)
+            }
         }
     }
 
@@ -30,17 +47,27 @@ class FootballWordCupScoreBoardTests {
     inner class FinishGame {
         @Test
         fun `archives current game`() {
-            //TODO
+            footballWordCupScoreBoard.currentGame = Game(team1Name, team2Name)
+
+            footballWordCupScoreBoard.finishGame()
+
+            assertThat(footballWordCupScoreBoard.archive, IsNot(IsEmptyCollection()))
         }
 
         @Test
         fun `un-assigns current game`() {
-            //TODO
+            footballWordCupScoreBoard.currentGame = Game(team1Name, team2Name)
+
+            footballWordCupScoreBoard.finishGame()
+
+            assertNull(footballWordCupScoreBoard.currentGame)
         }
 
         @Test
-        fun `throws if IllegalStateException game not running`() {
-            //TODO
+        fun `throws IllegalStateException if game not running`() {
+            assertThrows<IllegalStateException> {
+                footballWordCupScoreBoard.finishGame()
+            }
         }
     }
 
@@ -48,13 +75,17 @@ class FootballWordCupScoreBoardTests {
     inner class Update {
         @Test
         fun `throws IllegalArgumentException for negative values`() {
-            //TODO
+            assertThrows<IllegalArgumentException> {
+                footballWordCupScoreBoard.update(-1 to -2)
+            }
 
         }
 
         @Test
-        fun `throws if IllegalStateException game not running`() {
-            //TODO
+        fun `throws IllegalStateException if game not running`() {
+            assertThrows<IllegalStateException> {
+                footballWordCupScoreBoard.update(0 to 0)
+            }
         }
     }
 
@@ -62,12 +93,27 @@ class FootballWordCupScoreBoardTests {
     inner class GetGamesByTotalScore {
         @Test
         fun `no games returns empty set`() {
-            //TODO
+            val result = footballWordCupScoreBoard.getGamesOrderedByTotalScore()
+
+            assertThat(result, IsEmptyCollection())
         }
 
         @Test
         fun `result ordered by total score`() {
-            //TODO
+            fun getGame(homeTeamScore: Int, awayTeamScore: Int) =
+                Game(team1Name, team2Name).also {
+                    it.homeTeamScore.score = homeTeamScore
+                    it.awayTeamScore.score = awayTeamScore
+                }
+
+            val game1 = getGame(5, 5)
+            val game2 = getGame(1, 2)
+            val game3 = getGame(4, 3)
+
+            footballWordCupScoreBoard.archive.addAll(listOf(game1, game2, game3))
+            val result = footballWordCupScoreBoard.getGamesOrderedByTotalScore()
+
+            assertThat(result, containsInRelativeOrder(game1, game3, game2))
         }
     }
 
